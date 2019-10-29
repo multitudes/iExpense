d809a3b68b0c0416d48b2f80b559eb0d17e965da
laurent <wrmultitudes@gmail.com>
2019-10-29 17:24:26 +0100

    done

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var numbers = [Int]()
+    @State var currentNumber = 1
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                            Text("\($0)")
+                    }.onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+                
+                
+            }.navigationBarItems(leading: EditButton())
+        }
+    }
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
